031a4933279ebb608b6e8e4350c92b6532ebe086
EduardoFernandezC <46087668+EduardoFernandezC@users.noreply.github.com>
2019-04-24 17:37:05 +0200

    GDH_5 and GDH_10

---
+++ R/GDH_5.R
@@ -0,0 +1,17 @@
+GDH_5 <- function (HourTemp, summ = TRUE) 
+{
+  Stress <- 1
+  Tb <- 5
+  Tu <- 25
+  Tc <- 36
+  GDH_weight <- rep(0, length(HourTemp))
+  GDH_weight[which(HourTemp >= Tb & HourTemp <= Tu)] <- Stress * 
+    (Tu - Tb)/2 * (1 + cos(pi + pi * (HourTemp[which(HourTemp >= 
+                                                       Tb & HourTemp <= Tu)] - Tb)/(Tu - Tb)))
+  GDH_weight[which(HourTemp > Tu & HourTemp <= Tc)] <- Stress * 
+    (Tu - Tb) * (1 + cos(pi/2 + pi/2 * (HourTemp[which(HourTemp > 
+                                                         Tu & HourTemp <= Tc)] - Tu)/(Tc - Tu)))
+  if (summ) 
+    return(cumsum(GDH_weight))
+  else return(GDH_weight)
+}
