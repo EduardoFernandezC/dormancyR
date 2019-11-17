fc7fc7e9a12715f83076b98c0d67cf3f149c24fa
EduardoFernandezC <46087668+EduardoFernandezC@users.noreply.github.com>
2019-11-17 21:43:48 +0100

    function to compute percentage of data complete

---
+++ R/perc_complete.R
@@ -0,0 +1,36 @@
+#' Compute the percentage of data complete within a dataframe
+#' 
+#' Sometimes it is important to compute the percentage of data complete in a dataframe to have an idea of the 
+#' quality of the data. This is specially important for weather data where climate databases often have missing 
+#' days. This function only works for dataframes. For lists, see details.
+#' 
+#' @param x a dataframe normally produced through weather functions retrieving climate data. However, this is also
+#' possible to use with any dataframe.
+#' 
+#' @details For list of dataframes, it is useful to combine this function with lapply(). The output is a dataframe
+#' containing two columns (variable and percentage) and rows equal to the number of columns in the input dataframe.
+#' 
+#' @examples
+#' 
+#' df <- data.frame(year = rep(2011, 5),
+#'                  month = rep(1, 5),
+#'                  day = seq(1, 5),
+#'                  Tmin = c(10, 12, 11, NA, 21))
+#'                  
+#' perc_complete(df)           
+#' 
+#' @export perc_complete
+#' 
+
+
+perc_complete <- function(x){
+  
+  data <- data.frame(variable = colnames(x),
+                     percentage = NA)
+  
+  for (i in 1 : length(data$variable)){
+    
+    data[i, "percentage"] <- length(which(!is.na(x[, i]))) / length(x[, i]) * 100}
+  
+  
+  return(data)}
