afb1f947b408a08685910885f6886e3818626bfa
EduardoFernandezC <46087668+EduardoFernandezC@users.noreply.github.com>
2019-12-19 16:06:26 +0100

    enhance vignette and data issues

---
+++ R/dormancy.R
@@ -0,0 +1,11 @@
+#' dormancyR: a package to compute chill metrics from organized weather dataframes
+#' 
+#' The dormancyR package (inspired in \code{\link{chillR}}) aims to provide alternatives to common chill models
+#' used in horticulture to compute chill metrics in deciduous fruit tree orchards as well as functions to handle
+#' weather data.
+#' 
+#' @docType package
+#' @name dormancyR-package
+#' 
+#' @author Eduardo Fernandez
+NULL
