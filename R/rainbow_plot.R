c297c9ad4499121b48a8f350dcdd5b1e1e6cf9c5
EduardoFernandezC <46087668+EduardoFernandezC@users.noreply.github.com>
2020-11-27 13:50:11 +0100

    add customization to rainbow_plot() function

--- R/rainbow_plot.R
+++ R/rainbow_plot.R
@@ -2,18 +2,18 @@
 #' 
 #' The timing of many developmental stages of temperate trees may depend on temperatures during two
 #' phases (e.g. bloom dates depend on the temperature during both the chilling and forcing phase of dormancy).
-#' `rainbow_plot()` illustrates this dependency as a colored surface with contour lines by applying an
+#' \code{rainbow_plot()} illustrates this dependency as a colored surface with contour lines by applying an
 #' interpolating procedure with functions in the \code{\link{fields}} package. The plot is implemented
 #' through functions in the \code{\link{ggplot2}} package.
 #' 
 #' @param pheno_data is a data frame that contains information on the timing of phenological events by year.
-#' It should consist of two columns called `Year` and `pheno`. Data in the `pheno` column should be in
-#' Julian date (day of the year)
+#' It should consist of two columns called \code{Year} and \code{pheno}. Data in the \code{pheno} column should be in
+#' Julian date (day of the year).
 #' 
 #' @param weather_data is a dataframe containing daily minimum and maximum temperature data (in columns
-#' called `Tmin` and `Tmax`, respectively). There also has to be a column for `Year` and one for `JDay`
-#' (the Julian date, or day of the year). Alternatively, the date can also be given in three columns
-#' (`Year`, `Month` and `Day`)
+#' called \code{Tmin} and \code{Tmax}, respectively). There also has to be a column for \code{Year}, one for
+#' \code{Month} and one for \code{Day}. It can also contain a column for \code{JDay}
+#' (the Julian date, or day of the year).
 #' 
 #' @param split_month is an integer representing the last month of the growing season. This procedure analyzes
 #' data by phenological year, which can start and end in any month during the calendar year (currently only
@@ -28,32 +28,145 @@
 #' in temperate trees. Numbers must be provided in Julian date (day of the year).
 #' 
 #' @param Krig_warn is a boolean parameter passed to the \code{\link[fields:Krig]{Krig}} function. Default is
-#' set `TRUE` following the recomendation of the authors. For detailed information, please see the documentation
+#' set to \code{TRUE} following the recommendation of the authors. For detailed information, please see the documentation
 #' of the function.
+#' 
+#' @param x_axis_name is a character string that allows the user modifying the default label used in the x axis.
+#' 
+#' @param y_axis_name is a character string that allows the user modifying the default label used in the y axis.
+#' 
+#' @param legend_name is a character string that allows the user modifying the default label used in the legend.
+#' 
+#' @param contour_line_color is a character string representing the color used to draw the contour lines.
+#' Default is set to black. If \code{NA} is used, the function will remove the contour lines with a warning.
+#' 
+#' @param point_color is a character string representing the color used to draw the points for actual
+#' observations. Default is set to black. If \code{NA} is used, the function will remove the points with
+#' a warning.
+#' 
+#' @param point_shape is a numeric input representing the point shape used to draw the points for actual
+#' observations. Default is set to 19 (filled point). If \code{NA} is used, the function will remove the points with
+#' a warning.
+#' 
+#' @param legend_colors is a character string representing the color scale used in the surface plot.
+#' Default is set to \code{NULL} to let the function use the rainbow colors.
+#' 
+#' @param base_size is a numeric input representing the relative size of the elements in plot. \code{base_size}
+#' is passed to \code{\link[ggplot2:ggtheme]{ggplot2::theme_bw}} as well as used to determine the size of the
+#' points and contour lines.
+#' 
+#' @param ... accepts arguments passed to \code{\link[ggplot2:theme]{ggplot2::theme}} 
+#' 
 #' 
 #' @details The generation of the color surface is based on the Kriging technique, which is typically used for 
 #' interpolation of spatial data. The use for this particular purpose is a bit experimental.
 #' 
-#' @return `rainbow_plot()` is expected to return a plot as the following
+#' @return \code{rainbow_plot()} is expected to return an object of class \code{gg} and \code{ggplot}. This
+#' means that the plot can be later modified by using the syntax \code{'+'} from the
+#' \code{\link{ggplot2}} package (see examples). The plot returned in the function should look as the following:
 #' 
 #' \figure{rainbow_plot_example.png}{options: align='bottom' width='100\%' alt='rainbow_plot example'}
 #' 
 #' @examples 
-#'  
+#' 
+#' # Run a simple plot
+#' 
 #' rainbow_plot(pheno_data = chillR::KA_bloom,
 #'              weather_data = chillR::KA_weather,
 #'              chilling_phase = c(306, 350),
 #'              forcing_phase = c(355, 60))
+#'              
+#' # Customize the aspects of the plot and save it as 'plot'
+#' 
+#' plot <- rainbow_plot(pheno_data = chillR::KA_bloom,
+#'                      weather_data = chillR::KA_weather,
+#'                      chilling_phase = c(306, 350),
+#'                      forcing_phase = c(355, 60),
+#'                      x_axis_name = "Temperatura en el periodo de frio (Celsius)",
+#'                      y_axis_name = "Temperatura en el periodo de forzado (Celsius)",
+#'                      legend_name = "Fecha de floracion\n(dia juliano)",
+#'                      contour_line_color = "white",
+#'                      point_color = "blue4",
+#'                      point_shape = 4,
+#'                      legend_colors = NULL,
+#'                      base_size = 14,
+#'                      legend.position = "bottom",
+#'                      axis.title = ggplot2::element_text(family = "serif"))
+#' 
+#' plot
+#' 
+#' # Modify the plot object with the syntax from ggplot2.
+#' # Be aware that the following code overrides the modifications
+#' # done by the argument '...' in the main function
+#' 
+#' plot + ggplot2::theme_classic(base_size = 14)
 #' 
 #' @export rainbow_plot
 
-rainbow_plot <- function(pheno_data, weather_data, split_month = 6, chilling_phase, forcing_phase,
-                         Krig_warn = TRUE){
+rainbow_plot <- function(pheno_data,
+                         weather_data,
+                         split_month = 6,
+                         chilling_phase,
+                         forcing_phase,
+                         Krig_warn = TRUE,
+                         x_axis_name = NULL,
+                         y_axis_name = NULL,
+                         legend_name = NULL,
+                         contour_line_color = "black",
+                         point_color = "black",
+                         point_shape = 19,
+                         legend_colors = NULL,
+                         base_size = 11,
+                         ...){
   
   # REMEMBER TO ADD MORE CHECKS AND POSSIBLY THE CLASS THING
   
+  # Check the class of pheno dataframe
+  assertthat::assert_that("data.frame" %in% class(pheno_data),
+                          msg = "The 'pheno_data' input must be an element of class data.frame, please use a valid argument.")
+  
+  # Check colnames in the pheno dataframe
+  assertthat::assert_that(all(c("Year", "pheno") %in% colnames(pheno_data)),
+                          msg = "The 'pheno_data' input must be an element of class data.frame, please use a valid argument.")
+  
+  # Check the pheno column in the pheno dataset and transform it to numeric in case this is TRUE
   if (!is.numeric(pheno_data$pheno)) 
     pheno_data$pheno <- suppressWarnings(as.numeric(as.character(pheno_data$pheno)))
+  
+  
+  # Check the class of weather dataframe
+  assertthat::assert_that("data.frame" %in% class(weather_data),
+                          msg = "The 'weather_data' input must be an element of class data.frame, please use a valid argument.")
+  
+  # Check colnames in the weather dataframe
+  assertthat::assert_that(all(c("Year", "Month", "Day") %in% colnames(weather_data)),
+                          msg = "The 'weather_data' input must be an element of class data.frame, please use a valid argument.")
+  
+  # Check the columns in the weather dataset and transform it to numeric in case this is TRUE
+  for (colname in c("Year", "Month", "Day")){
+    if (!is.numeric(weather_data[[colname]])){
+      warning(paste("The column", paste0("'", colname, "'"), "in the weather dataframe is not numeric. The function will coerce this column into\n numeric using the function 'as.numeric()'. Please be aware that this function may introduce NA's in the dataframe"))
+    }
+    
+    weather_data[[colname]] <- suppressWarnings(as.numeric(as.character(weather_data[[colname]])))
+  }
+  
+  # Check split month
+  assertthat::assert_that(is.numeric(split_month) & split_month %in% 1 : 12,
+                          msg = "'split_month' must be a numeric input (integer) between 1 and 12.\n Please provide a valid argument.")
+  
+  # Check the chilling phase
+  assertthat::assert_that(is.numeric(chilling_phase) & all(chilling_phase %in% 1 : 366),
+                          msg = "'chilling_phase' must be a numeric vector input (integers) with values in the range 1 and 366.\n Please provide a valid argument.")
+  
+  
+  # Check the forcing phase
+  assertthat::assert_that(is.numeric(forcing_phase) & all(forcing_phase %in% 1 : 366),
+                          msg = "'forcing_phase' must be a numeric vector input (integers) with values in the range 1 and 366.\n Please provide a valid argument.")
+  
+  
+  
+  
   
   
   # Define the seasons in the weather dataframe according to the split_month argument
@@ -80,8 +193,8 @@ rainbow_plot <- function(pheno_data, weather_data, split_month = 6, chilling_pha
   weather_data <- weather_data[weather_data$Year %in% common_seasons, ]
   
   pheno_data <- pheno_data[pheno_data$Year %in% common_seasons, ]
-
-
+  
+  
   # Chilling and forcing phases
   
   if (chilling_phase[1] > chilling_phase[2])
@@ -122,7 +235,7 @@ rainbow_plot <- function(pheno_data, weather_data, split_month = 6, chilling_pha
         
         chilling_period <- dplyr::bind_rows(chilling_period, chill_season) else
           chilling_period <- chill_season}
-    
+  
   
   # Same for the forcing period
   
@@ -132,7 +245,7 @@ rainbow_plot <- function(pheno_data, weather_data, split_month = 6, chilling_pha
     
     
     forcing_season <- weather_data[weather_data$Season == common_season &
-                                      weather_data$JDay %in% forcing_days, ]
+                                     weather_data$JDay %in% forcing_days, ]
     
     if (nrow(forcing_season) == length(forcing_days) + 1 |
         nrow(forcing_season) == length(forcing_days) |
@@ -142,20 +255,20 @@ rainbow_plot <- function(pheno_data, weather_data, split_month = 6, chilling_pha
         
         forcing_period <- dplyr::bind_rows(forcing_period, forcing_season) else
           forcing_period <- forcing_season}
-    
+  
   
   # Summarize the data to obtain the mean temperature during the chilling and forcing phase respectively
   
   chilling_period <- suppressMessages(chilling_period %>% dplyr::mutate(Tmean = (Tmin + Tmax) /2) %>% 
-                                      dplyr::group_by(Season) %>% 
-                                      dplyr::summarise(Tmean_chilling_period = mean(Tmean)))
+                                        dplyr::group_by(Season) %>% 
+                                        dplyr::summarise(Tmean_chilling_period = mean(Tmean)))
   
   
   # The same for forcing data frame
   
   forcing_period <- suppressMessages(forcing_period %>% dplyr::mutate(Tmean = (Tmin + Tmax) /2) %>%  
-                                     dplyr::group_by(Season) %>% 
-                                     dplyr::summarise(Tmean_forcing_period = mean(Tmean)))
+                                       dplyr::group_by(Season) %>% 
+                                       dplyr::summarise(Tmean_forcing_period = mean(Tmean)))
   
   
   # Merge both data frames 
@@ -167,7 +280,7 @@ rainbow_plot <- function(pheno_data, weather_data, split_month = 6, chilling_pha
   
   mean_temp_phase <- dplyr::left_join(mean_temp_phase, pheno_data, by = c("Season" = "Year"))
   
-
+  
   # Remove NAs for kriging surface
   
   mean_temp_phase <- stats::na.omit(mean_temp_phase)
@@ -180,7 +293,7 @@ rainbow_plot <- function(pheno_data, weather_data, split_month = 6, chilling_pha
                                        Y = mean_temp_phase$pheno,
                                        give.warnings = Krig_warn)
   
- 
+  
   # Rainbow plot with ggplot2
   
   # Predict the surface from the Krig element
@@ -220,32 +333,48 @@ rainbow_plot <- function(pheno_data, weather_data, split_month = 6, chilling_pha
   fill_data <- stats::na.omit(fill_data)
   
   
+  # Define some customizations to be used in the plot
+  
+  # Legend colors
+  if (is.null(legend_colors)) legend_colors <- c("darkblue", "blue", "cyan2",
+                                                 "green2", "yellow", "red", "red4")
+  
+  # x_axis_name
+  if (is.null(x_axis_name)) 
+    x_axis_name <- paste0("Mean temperature during the chilling phase (", "\U00B0", "C)")
+  
+  # y_axis_name
+  if (is.null(y_axis_name)) 
+    y_axis_name <- paste0("Mean temperature during the forcing phase (", "\U00B0", "C)")
+  
+  # legend_name
+  if (is.null(legend_name)) 
+    legend_name <- "Bloom date\n(DOY)"
+  
   # Plot
   
   plot <- ggplot2::ggplot(fill_data, ggplot2::aes(Chilling, Forcing)) +
     ggplot2::geom_raster(ggplot2::aes(fill = Bloom)) +
-    ggplot2::geom_contour(ggplot2::aes(z = Bloom), data = fill_data, size = 0.7,
-                          color = "black") +
+    ggplot2::geom_contour(ggplot2::aes(z = Bloom), data = fill_data, size = base_size / 25,
+                          color = contour_line_color) +
     metR::geom_text_contour(ggplot2::aes(z = Bloom), skip = 1,
                             data = fill_data, rotate = FALSE, stroke = 0.1,
-                            stroke.color = "white") +
+                            stroke.color = "white", size = base_size * 0.3) +
     ggplot2::scale_y_continuous(expand = ggplot2::expansion(add = 0.1)) +
     ggplot2::scale_x_continuous(expand = ggplot2::expansion(add = 0.1)) +
-    ggplot2::scale_fill_gradientn(colors = c("darkblue", "blue", "cyan2",
-                                             "green2", "yellow", "red", "red4")) +
+    ggplot2::scale_fill_gradientn(colors = legend_colors) +
     ggplot2::geom_point(ggplot2::aes(Tmean_chilling_period,
                                      Tmean_forcing_period), data = mean_temp_phase,
-                        color = "black") +
-    ggplot2::labs(x = paste0("Mean temperature during the chilling phase (",
-                            "\U00B0", "C)"),
-                  y = paste0("Mean temperature during the Forcing phase (",
-                             "\U00B0", "C)"),
-                  fill = "Bloom date\n(DOY)") +
-    ggplot2::theme_bw() +
-    ggplot2::theme(legend.title.align = 0.5)
+                        color = point_color,
+                        size = base_size / 8,
+                        shape = point_shape) +
+    ggplot2::labs(x = x_axis_name,
+                  y = y_axis_name,
+                  fill = legend_name) +
+    ggplot2::theme_bw(base_size = base_size) +
+    ggplot2::theme(legend.title.align = 0.5) +
+    ggplot2::theme(...)
   
   return(plot)
-
+  
 }
-
-
