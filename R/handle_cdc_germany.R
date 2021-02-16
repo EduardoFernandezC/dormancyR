135c27221040e8afba313206878b1d8cc4a40903
EduardoFernandezC <46087668+EduardoFernandezC@users.noreply.github.com>
2021-02-16 02:08:33 +0100

    update handle_cdc_germany

--- R/handle_cdc_germany.R
+++ R/handle_cdc_germany.R
@@ -2,16 +2,13 @@
 #' 
 #' This function access the \href{https://www.dwd.de/EN/climate_environment/cdc/cdc_node.html}{Climate Data Center - CDC} 
 #' and allows to:\itemize{\item{1) obtain information of the weather stations from Germany.}
-#' \item{2) get weather data for a specific place of interest given the coordinates.}
-#' \item{3) get weather data from several weather stations located close to a specific location
-#'  given its coordinates.}}
+#' \item{2) get weather data for one or more specific places of interest given the station ID.}}
 #'    
-#' @param action is a character string to decide on 3 modes of action for the function.
+#' @param action is a character string to decide on 2 modes of action for the function.
 #' \emph{"list_stations"} returns a dataframe with the information on close weather stations
 #' to the location defined by \code{number_of_stations} and \code{longitude} and \code{latitude} parameters.
-#' \emph{"download_weather"} retrieve the records for the closest weather station to the defined location.
-#' \emph{"download_weather_list"} downloads the records for several weather stations (according to
-#' \code{number_of_stations}) which are close to the location of interest.
+#' \emph{"download_weather"} retrieve the records for one or more weather stations defined in the
+#' \code{station_ID} parameter.
 #' 
 #' @param variables is a character vector representing the variables required. For now, the function can return the
 #' wind speed (mean - \emph{"Wind_speed"} and maximum - \emph{"Wind_speed_max"}); the atmospheric pressure
@@ -24,6 +21,8 @@
 #' 
 #' @param longitude is a numeric parameter defining the longitude (in decimal degrees) of the location of interest.
 #' 
+#' @param station_ID is a character vector representing the stations to be used for downloading the data
+#' 
 #' @param begin is a numeric parameter representing the start date for the period required. This must be
 #' specified in YEARMODA format.
 #' 
@@ -32,35 +31,55 @@
 #' 
 #' @param number_of_stations is a numeric parameter defininf the numbers of stations to select from.
 #' 
-#' @param complete_list is a boolean parameter. If \emph{"download_weather"} option has been used, this allows to skip the first
-#' weather station when using the \emph{"download_weather_list"} option. It avoids include a repeated dataframe for the
-#' first weather station
+#' @param add.DATE is a boolean parameter to be passed to \code{\link{make_all_day_table}}
+#' 
+#' @param quiet is a boolean parameter to be passed to \code{\link[utils:download.file]{download.file}}
 #'  
 #' @details If \emph{"list_stations"} is used, the function returns a dataframe (9 columns x \code{number_of_stations} rows) containing
 #' information such as the name, latitude, longitude, begin, end and distance of the weather stations.
+#' \code{latitude} and \code{longitude} must be provided.
 #' If \emph{"download_weather"} is chosen, it downloads the weather data from the CDC website. The output is
 #' a dataframe (in \code{\link{chillR}} format) containing daily records from the closest
-#' weather station. If \emph{"download_weather_list"} option is used, the function returns a list of dataframes as the one
-#' described above. The length of the list is equal to the \code{number_of_stations} or to the \code{number_of_stations}
-#' minus 1 if \code{complete_list = FALSE}.
+#' weather station. \code{station_ID} must be provided. If \code{station_ID} is longer than one, the function returns a list of dataframes as the one
+#' described above.
 #' 
 #' @examples 
 #' 
 #' handle_cdc_germany(action = "list_stations", variables = c("Tmin", "Tmax", "Tmean"),
 #'                    latitude = 53.5373, longitude = 9.6397, begin = 20000101,
-#'                    end = 20101231, number_of_stations = 25, complete_list = FALSE)
+#'                    end = 20101231, number_of_stations = 25)
 #' 
 #' @export handle_cdc_germany
 
-handle_cdc_germany <- function(action, variables,  latitude, longitude,
-                               begin = 19160101, end = chillR::Date2YEARMODA(Sys.Date()),
+handle_cdc_germany <- function(action,
+                               variables,
+                               latitude = NULL,
+                               longitude = NULL,
+                               station_ID = NULL,
+                               begin = 19160101,
+                               end = chillR::Date2YEARMODA(Sys.Date()),
                                number_of_stations = 25,
-                               complete_list = FALSE){
+                               add.DATE = FALSE, 
+                               quiet = FALSE){
   
   # Checking if action, dates and variables are valid inputs
   
-  if (!action %in% c("list_stations", "download_weather", "download_weather_list")) 
+  if (!action %in% c("list_stations", "download_weather")) 
     stop("Please provide a valid action")
+  
+  if (action == "list_stations"){
+    assertthat::assert_that(!is.null(latitude) & !is.null(longitude),
+                            msg = "Please provide a valid input for latitude and longitude of the place of interest")
+    
+    assertthat::assert_that(is.null(station_ID),
+                            msg = "Please set 'station_ID' to NULL or set the 'action' to download_weather")}
+  
+  if (action == "download_weather"){
+    assertthat::assert_that(!is.null(station_ID),
+                            msg = "Plase provide at least one station_ID to download data from")
+    
+    assertthat::assert_that(is.null(latitude) & is.null(longitude),
+                            msg = "Please set 'latitude' and 'longitude' to NULL or set the 'action' to list_stations")}
   
   if (nchar(begin) != 8 | nchar(end) != 8)
     stop("Invalid date for begin or end parameters. Please introduce a date in format YEARMODA")
@@ -137,29 +156,41 @@ handle_cdc_germany <- function(action, variables,  latitude, longitude,
   
   stations <- dplyr::inner_join(stations, zip_files, by = "Station_ID")
   
-  # Add the distance to the location of interest
-  
-  myPoint <- c(longitude, latitude)
-  
-  stations[, "Distance"] <- round(sp::spDistsN1(
-    as.matrix(stations[, c("Longitude", "Latitude")]), myPoint, longlat = TRUE), 2) 
-  
-  # Order the station according to the distance to the point
-  
-  stations_sorted <- stations[order(stations$Distance), ]
   
   # Remove those stations which end the record before the begin or those which start after the end
   # defined in the call of the function 
   
-  station_in_period <- stations_sorted[-which(stations_sorted$End < begin | stations_sorted$Begin > end),]
+  station_in_period <- stations[-which(stations$End < begin | stations$Begin > end), ]
   
   # If the procedure above results in no station selected just return the sorted list
   
-  if (length(station_in_period$Station_name) == 0) station_in_period <- stations_sorted
+  if (length(station_in_period$Station_name) == 0){
+    
+    station_in_period <- stations
+    warning("No stations selected for the period of interest. The complete list will be provided")}
   
   
-  if (action == "list_stations")
-    return(station_in_period[c(1: number_of_stations),])
+  # In case the action is set to list_stations, this will compute the distance to the location of interest.
+  # Otherwise, the distance is not important, but I need the dataframe of stations to downloading the data
+  
+  if (action == "list_stations"){
+    
+    # Add the distance to the location of interest
+    
+    myPoint <- c(longitude, latitude)
+    
+    station_in_period[, "Distance"] <- round(sp::spDistsN1(
+      as.matrix(station_in_period[, c("Longitude", "Latitude")]), myPoint, longlat = TRUE), 2) 
+    
+    # Order the station according to the distance to the point
+    
+    stations_sorted <- station_in_period[order(station_in_period$Distance), ]
+    
+    # Return of the function
+    
+    return(stations_sorted[c(1 : number_of_stations), ])}
+  
+  
   
   
   # primer dataframe to include the complete period of interest
@@ -174,7 +205,7 @@ handle_cdc_germany <- function(action, variables,  latitude, longitude,
   
   # Add all rows for the period
   
-  primer <- chillR::make_all_day_table(primer)
+  primer <- chillR::make_all_day_table(primer, add.DATE = add.DATE)
   
   # Add YEARMODA value for future use in merging dataframes
   
@@ -184,7 +215,11 @@ handle_cdc_germany <- function(action, variables,  latitude, longitude,
   
   # Remove Tmin, Tmax and Tmean from the primer dataframe for make it clearer
   
-  primer <- primer[, c("DATE", "YEARMODA", "Year", "Month", "Day")]
+  if (add.DATE){
+    
+    primer <- primer[, c("DATE", "YEARMODA", "Year", "Month", "Day")]} else {
+      
+      primer <- primer[, c("YEARMODA", "Year", "Month", "Day")]}
   
   
   # Downloading the data. Specify the URL where the data is 
@@ -195,12 +230,10 @@ handle_cdc_germany <- function(action, variables,  latitude, longitude,
   
   if(!dir.exists("tempdir")) dir.create("tempdir")
   
-  # Depending on the action selected when calling the function this allows to get the data for just 1
-  # (download_weather) or the total number of stations selected (download_weather_list). It saves a bit of time when the 
-  # download_weather option is used
+  # Depending on the number of elements in the station_ID parameter, this will download the data as a list 
+  # or a data frame
   
-  if (action == "download_weather") number_of_stations <- 1 
-  if (action == "download_weather_list") number_of_stations <- number_of_stations
+  if (action == "download_weather") stations_to_download <- as.character(station_ID)
   
   # Variable names in the original dataframe
   
@@ -209,28 +242,44 @@ handle_cdc_germany <- function(action, variables,  latitude, longitude,
   # Get the data for the number of stations used by calling the function
   
   download_weather_list <- list()
-  for (i in 1 : number_of_stations) {
+  for (i in 1 : length(stations_to_download)) {
     
     # URL of the individual station
     
-    URL <- paste(master_URL, "tageswerte_KL_", station_in_period[i, "Station_ID"], "_",
-                 station_in_period[i, "Begin"], "_", station_in_period[i, "End"], "_hist.zip", sep = "")
+    URL <- paste(master_URL,
+                 "tageswerte_KL_",
+                 stations_to_download[i],
+                 "_",
+                 station_in_period[which(station_in_period$Station_ID == stations_to_download[i]), "Begin"],
+                 "_",
+                 station_in_period[which(station_in_period$Station_ID == stations_to_download[i]), "End"],
+                 "_hist.zip",
+                 sep = "")
     
     # Download the zip file
     
-    utils::download.file(URL, destfile = "tempdir/data.zip")
+    utils::download.file(URL, destfile = "tempdir/data.zip", quiet = quiet)
     
     # Extract, in the temporary directory, just the file containing the data
     
-    utils::unzip("tempdir/data.zip", files = paste("produkt_klima_tag_", station_in_period[i, "Begin"], "_",
-                                                   station_in_period[i, "End"], "_",
-                                                   station_in_period[i, "Station_ID"], ".txt", sep = ""),
+    utils::unzip("tempdir/data.zip", files = paste("produkt_klima_tag_",
+                                                   station_in_period[which(station_in_period$Station_ID == stations_to_download[i]), "Begin"],
+                                                   "_",
+                                                   station_in_period[which(station_in_period$Station_ID == stations_to_download[i]), "End"],
+                                                   "_",
+                                                   stations_to_download[i],
+                                                   ".txt",
+                                                   sep = ""),
                  exdir = "tempdir")
     
     # Import such file. It keeps all the columns
     
-    data <- utils::read.csv(paste("tempdir/produkt_klima_tag_", station_in_period[i, "Begin"], "_",
-                                  station_in_period[i, "End"], "_", station_in_period[i, "Station_ID"],
+    data <- utils::read.csv(paste("tempdir/produkt_klima_tag_",
+                                  station_in_period[which(station_in_period$Station_ID == stations_to_download[i]), "Begin"],
+                                  "_",
+                                  station_in_period[which(station_in_period$Station_ID == stations_to_download[i]), "End"],
+                                  "_",
+                                  stations_to_download[i],
                                   ".txt", sep = ""), sep = ";",
                             colClasses = c("numeric", "numeric", rep("character", length(variables_gers))), 
                             na.strings = "-999")[c("STATIONS_ID", "MESS_DATUM", variables_gers)]
@@ -258,12 +307,17 @@ handle_cdc_germany <- function(action, variables,  latitude, longitude,
     
     # Add station name and station ID to the data
     
-    data["Station_name"] <- as.character(station_in_period[i, "Station_name"])
-    data["Station_ID"] <- as.character(station_in_period[i, "Station_ID"])
+    data["Station_name"] <- as.character(station_in_period[which(station_in_period$Station_ID == stations_to_download[i]), "Station_name"])
+    data["Station_ID"] <- stations_to_download[i]
     
     # Order the data by columns and variables selected by calling the function
     
-    data <- data[, c("Station_name", "Station_ID", "DATE", "YEARMODA", "Year", "Month", "Day", variables)]
+    if (add.DATE){
+      
+      data <- data[, c("Station_name", "Station_ID", "DATE", "YEARMODA", "Year", "Month", "Day", variables)]} else {
+        
+        data <- data[, c("Station_name", "Station_ID", "YEARMODA", "Year", "Month", "Day", variables)]}
+    
     
     data <- list(data)
     
@@ -273,8 +327,7 @@ handle_cdc_germany <- function(action, variables,  latitude, longitude,
   
   unlink("tempdir", recursive = TRUE, force = TRUE)
   
-  if(action == "download_weather")
-    return(download_weather_list[[1]])
+  # Return the downloaded data 
   
-  if(action == "download_weather_list" & complete_list == TRUE)
-    return(download_weather_list) else return(download_weather_list[-1])}
+  if(length(download_weather_list) == 1)
+    return(download_weather_list[[1]]) else return(download_weather_list)}
